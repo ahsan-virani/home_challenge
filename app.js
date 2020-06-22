7edc9055f1b4f5c53243bd4e337361aab13becf5
ahsan-virani <ahsan.virani@gmail.com>
2020-06-22 03:25:33 +0200

    make runnable and testable

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 
 const express    = require("express"),
       bodyParser = require("body-parser"),
+      moment     = require("moment"),
       config     = require("./config/env"),
+      logger     = require("./lib/logger"),
       health     = require("./api/health"),
       routes     = require("./api/v1/routes");
 
@@ -13,5 +15,20 @@ app.use(health.healthCheckMiddleware);
 
 app.use("/api/v1", routes);
 app.use("/api/health", health.healthCheckHandler);
+
+process.on("uncaughtException", err => logger.error("Uncaught exception", err));
+process.on("unhandledRejection", (err, promise) => logger.error("Unhandled rejection - Promise: %j", promise, err));
+
+if (require.main === module) {
+  app.listen(config.port, config.ip, function () {
+    console.log("========================================================================");
+    console.log("");
+    console.log("  Starting Home Challenge Service on " + moment().format("HH:mm:ss DD-MM-YYYY"));
+    console.log("      Environment: " + config.env);
+    console.log("            Port: " + config.port);
+    console.log("");
+    console.log("========================================================================");
+  });
+}
 
 module.exports = { app };
